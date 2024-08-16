d6b1c98db8ff7302164d706908b6523963e9632f
codewithrohitt <141995104+codewithrohitt@users.noreply.github.com>
2024-08-16 14:29:32 +0530

    LMS By Rohit Singh

---
+++ contact.php
@@ -0,0 +1,28 @@
+<div class="container mt-4">
+        <h2 class="text-center mb-4">Contact Us</h2>
+        <div class="row">
+            <div class="col-md-8">
+                <form action="" method="post">
+                    <input type="text" class="form-control" name="name" placeholder="Name"> <br>
+                    <input type="text" class="form-control" name="subject" placeholder="Subject"> <br>
+                    <input type="email" class="form-control" name="E-mail" placeholder="E-mail"> <br>
+                    <textarea name="message" placeholder="How can we help you?" class="form-control"
+                        style="height:150px;"></textarea><br>
+                    <input type="submit" class="btn btn-primary" value="Send" name="submit"><br><br>
+                </form>
+            </div>
+
+
+
+
+            <div class="col-md-4 stripe text-white text-center">
+                <h4>Easy<FONT COLOR=red>Learn</h4>
+                </FONT>
+                <p>EasyLearn,
+                    Near Udhna Academy, Surat,
+                    Gujarat - 394210 <br />
+                    Phone: +123123123 <br />
+                    www.EasyLearn.com </p>
+            </div>
+        </div>
+    </div>
---
+++ index.php
@@ -0,0 +1,319 @@
+<?php 
+include('./mainInclude/header.php');
+?>
+
+    <!-- Background-->
+    <div class="container-fluid remove-vid-marg">
+        <div class="vid-parent">
+            <video playsinline autoplay muted loop>
+                <source src="video/video (2160p).mp4" type="">
+            </video>
+            <div class="vid-overlay"></div>
+        </div>
+        <div class="vid-content">
+            <h1 class="my-content">Welcome To Easy<FONT COLOR=red>Learn</FONT>
+            </h1>
+            <small class="my-content">HARD WORK ALWAYS WINS IN THE END</small><br><br>
+            <a href="#" class="btn btn-danger" data-toggle="modal" data-target="#stuRegModalCenter">EXPLORE OUR COURSES</a>
+
+        </div>
+    </div>
+    <!--end background -->
+
+    <!--start text banner -->
+    <div class="container-fluid bg-danger txt-banner">
+        <div class="row bottom-banner">
+            <div class="col-sm">
+                <h5><i class="fas fa-book-open mr-3"></i>100+ Online Courses</h5>
+            </div>
+            <div class="col-sm">
+                <h5><i class="fas fa-users mr-3"></i>Career Support</h5>
+            </div>
+            <div class="col-sm">
+                <h5><i class="fas fa-keyboard mr-3"></i>Lifetime Access</h5>
+            </div>
+            <div class="col-sm">
+                <h5><i class="fa-solid fa-indian-rupee-sign mr-3"></i>Money Back Guarantee* </h5>
+            </div>
+
+        </div>
+    </div>
+
+
+    <!--end text banner -->
+
+    <!-- Start Most Popular Courses -->
+
+
+    <div class="container mt-5">
+        <h1 class="text-center">Popular Course</h1>
+
+
+        <!-- Start Most Popular Courses 1st Card Deck-->
+        <div class="card-deck mt-4">
+            <a href="#" class="btn" style="text-align: left; padding:0px; margin:0px;">
+                <div class="card">
+                    <div class="top-right">70% off</div>
+                    <img src="image/HTML5-1024x768.jpg" class="card-img-top" alt="Guitar" height="230px" width="230px"
+                        style=" object-fit: cover;" />
+                    <div class="card-body">
+                        <h5 class="card-title"><b>
+                                <font color=black>HTML 5</font>
+                            </b></h5>
+                        <p class="card-textt">The Complete HTML Bootcamp From Zero To Hero <br> in HTML</p>
+                    </div>
+                    <div class="card-footer">
+                        <p class="card-text d-inline">Price: <small><del>&#8377 2,199</del></small><span
+                                class="font-weight-bolder">&nbsp; &#8377 659</span></p>
+                        <a href="#" class="btn btn-primary text-white font-weight-bolder float-right">Enroll Now</a>
+                    </div>
+                </div>
+            </a><a href="#" class="btn" style="text-align: left; padding:0px; margin:0px;">
+                <div class="card">
+                    <div class="top-right">75% off</div>
+                    <img src="image/CSS3-900x0.jpg" class="card-img-top" alt="Guitar" height="230px" width="230px"
+                        style=" object-fit: cover;" />
+                    <div class="card-body">
+                        <h5 class="card-title"><b>
+                                <font color=black>CSS 3</font>
+                            </b></h5>
+                        <p class="card-textt">Complete guide to create beautiful,<br> responsive and user-friendly
+                            websites using CSS3. </p>
+                    </div>
+                    <div class="card-footer">
+                        <p class="card-text d-inline">Price: <small><del>&#8377 3,499</del></small><span
+                                class="font-weight-bolder">&nbsp; &#8377 875</span></p>
+                        <a href="#" class="btn btn-primary text-white font-weight-bolder float-right">Enroll Now</a>
+                    </div>
+                </div>
+            </a><a href="#" class="btn" style="text-align: left; padding:0px; margin:0px;">
+                <div class="card">
+                    <div class="top-right">65% off</div>
+                    <img src="image/bootstrap-logo-vector.png" class="card-img-top" alt="Guitar" height="230px"
+                        width="230px" style=" object-fit: cover;" />
+                    <div class="card-body">
+                        <h5 class="card-title"><b>
+                                <font color=black>Bootstrap</font>
+                            </b></h5>
+                        <p class="card-textt">Learn to customize and build modern websites from <br> scratch using
+                            Bootstrap 5</p>
+                    </div>
+                    <div class="card-footer">
+                        <p class="card-text d-inline">Price: <small><del>&#8377 1,499</del></small><span
+                                class="font-weight-bolder">&nbsp; &#8377 525</span></p>
+                        <a href="#" class="btn btn-primary text-white font-weight-bolder float-right">Enroll Now</a>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <!-- End Most Popular Courses 1st Deck-->
+
+
+        <!-- Start Most Popular Courses 2nd Deck-->
+
+        <div class="card-deck mt-4">
+            <a href="#" class="btn" style="text-align: left; padding:0px;">
+                <div class="card">
+                    <div class="top-right"> 20% off</div>
+                    <img src="image/t_php.jpg" class="card-img-top" alt="Guitar" height="230px" width="230px"
+                        style=" object-fit: cover;" />
+                    <div class="card-body">
+                        <h5 class="card-title"><b>
+                                <font color=black>PHP</font>
+                            </b></h5>
+                        <p class="card-textt">Learn to create dynamic web applications using <br>PHP.</p>
+                    </div>
+                    <div class="card-footer">
+                        <p class="card-text d-inline">Price: <small><del>
+                                    &#8377 2,499</del></small><span class="font-weight-bolder">&nbsp; &#8377
+                                1,999</span></p>
+                        <a href="#" class="btn btn-primary text-white font-weight-bolder float-right">Enroll Now</a>
+                    </div>
+                </div>
+            </a>
+            <a href="#" class="btn" style="text-align: left; padding:0px; margin:0px;">
+                <div class="card">
+                    <div class="top-right">40% off</div>
+                    <img src="image/Azure-SQL-Database.png" class="card-img-top" alt="Guitar" height="230px"
+                        width="230px" style=" object-fit: cover;" />
+                    <div class="card-body">
+                        <h5 class="card-title"><b>
+                                <font color=black>SQL Database</font>
+                            </b></h5>
+                        <p class="card-textt">The Ultimate MySQL Bootcamp: Go from SQL <br>Beginner to Expert</p>
+                    </div>
+                    <div class="card-footer">
+                        <p class="card-text d-inline">Price: <small><del>
+                                    &#8377 2,349</del></small><span class="font-weight-bolder">&nbsp; &#8377
+                                1,409</span></p>
+                        <a href="#" class="btn btn-primary text-white font-weight-bolder float-right">Enroll Now</a>
+                    </div>
+                </div>
+            </a><a href="#" class="btn" style="text-align: left; padding:0px; margin:0px;">
+                <div class="card">
+                    <div class="top-right">40% off</div>
+                    <img src="image/Python-01.jpg" class="card-img-top" alt="Guitar" height="230px" width="230px"
+                        style=" object-fit: cover;" />
+                    <div class="card-body">
+                        <h5 class="card-title"><b>
+                                <font color=black>Python</font>
+                            </b></h5>
+                        <p class="card-textt">Learn Python Start from the basics and go all the way <br>to creating your
+                            own applications and games</p>
+                    </div>
+                    <div class="card-footer">
+                        <p class="card-text d-inline">Price: <small><del>&#8377 3,699</del></small><span
+                                class="font-weight-bolder">&nbsp; &#8377 2,199</span></p>
+                        <a href="#" class="btn btn-primary text-white font-weight-bolder float-right">Enroll Now</a>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <br>
+
+        <!-- End Most Popular Courses 2nd Deck-->
+
+        <div class="text-center m-2">
+            <a href="courses.php" class="btn btn-danger btn-bg">View All Course</a>
+        </div>
+
+    </div><br>
+
+    <!-- End Most Popular Courses -->
+
+    <!-- Start Contact Us-->
+
+    <?php
+    include('./contact.php');
+    ?>
+    <!-- End Contact Us-->
+
+
+    <!-- Start Students Testimonial -->
+    <div class="container-fluid mt-5" style="background-color: gray" id="Feedback">
+        <h1 class="text-center testyheading p-4" style="color: white"> Student's Feedback </h1>
+        <div class="row">
+            <div class="col-md-12">
+                <div id="testimonial-slider" class="owl-carousel">
+                    <div class="testimonial">
+                        <p class="description">
+                            "The course increased my practical knowledge in Agile methodology and helped me get promoted
+                            to a Project Manager in the short span of 1.5 months."
+                        </p>
+                        <div class="pic">
+                            <img src="image/men.jpg" alt="" />
+                        </div>
+                        <div class="testimonial-prof">
+                            <h4>Rohit</h4>
+                            <small>Web Developer </small>
+                        </div>
+                    </div>
+                    <div class="testimonial">
+                        <p class="description">
+                        "I never imagined that HTML could be so fun!! Incredible course, it opened my eyes in many ways. I discovered new ways of learning/teaching Full Stack Development. Sir you are my favorite."
+                        </p>
+                        <div class="pic">
+                            <img src="image/logo.jpg" alt="" />
+                        </div>
+                        <div class="testimonial-prof">
+                            <h4>Shubham</h4>
+                            <small>Web Designing </small>
+                        </div>
+                    </div>
+                    <div class="testimonial">
+                        <p class="description">
+                        "When a child starts to dream about his career he just need an environment where your hope turns into belief and this is the place." <br> &#128519;
+                        </p>
+                        <div class="pic">
+                            <img src="image/male1.jpg" alt="" />
+                        </div>
+                        <div class="testimonial-prof">
+                            <h4>Raj</h4>
+                            <small>Data Analysis </small>
+                        </div>
+                    </div>
+                    <div class="testimonial">
+                        <p class="description">
+                        "As a student if I want to give a review than I will say that EasyLearn is the best  tutorial in the Surat.
+                        So elegent, So Beautiful." <br>&#128540;
+                        </p>
+                        <div class="pic">
+                            <img src="image/female2.jpg" alt="" />
+                        </div>
+                        <div class="testimonial-prof">
+                            <h4>Aaradhya</h4>
+                            <small>Frontend Developer</small>
+                        </div>
+                    </div>
+                    <div class="testimonial">
+                        <p class="description">
+                        "Loved every minue studying and gained great knowledge. Looking forward to life as a life coach and being part of changing lives for the better."
+                        </p>
+                        <div class="pic">
+                            <img src="image/female.jpg" alt="" />
+                        </div>
+                        <div class="testimonial-prof">
+                            <h4>Dipti</h4>
+                            <small>App Developer </small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+
+    <!-- End Students Testimonial -->
+    <div class="container-fluid bg-danger">
+        <!-- Start Social Follow -->
+        <div class="row text-white text-center p-1">
+            <div class="col-sm">
+                <a class="text-white social-hover" href="https://www.facebook.com/profile.php?id=100053236154829&mibextid=LQQJ4d"><i class="fab fa-facebook-f"></i> Facebook</a>
+            </div>
+            <div class="col-sm">
+                <a class="text-white social-hover" href="https://twitter.com/mrrohitmohit45?lang=en"><i class="fab fa-twitter"></i> Twitter</a>
+            </div>
+            <div class="col-sm">
+                <a class="text-white social-hover" href="https://chat.whatsapp.com/I6J2HXAPJfKGIawrnqVDtC"><i class="fab fa-whatsapp"></i> WhatsApp</a>
+            </div>
+            <div class="col-sm">
+                <a class="text-white social-hover" href="https://www.instagram.com/rohitian_singh?igsh=NGVhN2U2NjQ0Yg%3D%3D&utm_source=qr"><i class="fab fa-instagram"></i> Instagram</a>
+            </div>
+        </div>
+    </div>
+    <!-- End Social Follow -->
+
+    <!-- Start About Section -->
+    <div class="container-fluid p-4" style="background-color:#E9ECEF">
+        <div class="container" style="background-color:#E9ECEF">
+            <div class="row text-center">
+                <div class="col-sm">
+                    <h5>About Us</h5>
+                    <p>EasyLearn provides universal access to the world’s best education, partnering with top
+                        universities and organizations to offer courses online.</p>
+                </div>
+                <div class="col-sm">
+                    <h5>Category</h5>
+                    <a class="text-dark" href="#">Web Development</a><br />
+                    <a class="text-dark" href="#">Web Designing</a><br />
+                    <a class="text-dark" href="#">Android App Dev</a><br />
+                    <a class="text-dark" href="#">iOS Development</a><br />
+                    <a class="text-dark" href="#">Data Analysis</a><br />
+                </div>
+                <div class="col-sm">
+                    <h5>Contact Us</h5>
+                    <p>EasyLearn Pvt Ltd <br> Near Udhna Academy, Surat, <br>
+                        Gujarat - 394210 <br />
+                        Phone: +123123123 <br />
+                        www.EasyLearn.com </p>
+                </div>
+            </div>
+        </div>
+    </div> <!-- End About Section -->
+
+
+<!-- Start including Footer -->
+<?php 
+include('./mainInclude/footer.php');
+?>
+<!-- End including Footer -->
